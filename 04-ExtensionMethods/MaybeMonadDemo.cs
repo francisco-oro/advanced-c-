2000979b8e5f0804397627f513c701f41dc3e5f6
francisco-oro <francisco.oro@comunidad.unam.mx>
2024-05-03 12:13:42 -0600

    Implemented MaybeMonad

--- 04-ExtensionMethods/MaybeMonadDemo.cs
+++ 04-ExtensionMethods/MaybeMonadDemo.cs
@@ -3,13 +3,50 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net.Sockets;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace ExtensionMethods
 {
     public static class Maybe
     {
-        public static TResult With<TInput, TResult>(this TInput o,  Func<TInput, TResult> func)
+        public static TResult With<TInput, TResult>(this TInput o, Func<TInput, TResult> evaluator)
+        where TResult : class
+        where TInput : class
+        {
+            if (o == null) return null;
+            else return evaluator(o);
+        }
+
+        public static TInput If<TInput>(this TInput o, Func<TInput, bool> evaluator)
+            where TInput : class
+        {
+            if (o == null) return null;
+            return evaluator(o) ? o : null;
+        }
+
+        public static TInput Do<TInput>(this TInput o, Action<TInput> action)
+            where TInput : class
+        {
+            if (o == null) return null;
+            action(o);
+            return o;
+        }
+
+
+        public static TResult Return<TInput, TResult>(this TInput o, Func<TInput, TResult> evaluator,
+            TResult failureValue)
+        where TInput : class
+        {
+            if(o == null) return failureValue;
+            return evaluator(o);
+        }
+
+        public static TResult WithValue<TInput, TResult>(this TInput o, Func<TInput, TResult> evaluator)
+        where TInput : struct
+        {
+            return evaluator(o);
+        }
     }
 
     public partial class Person
@@ -42,21 +79,37 @@ namespace ExtensionMethods
                     }
                 }
             }
+
+            postcode = p.With(x => x.Address).With(x => x.PostCode);
+
+            postcode = p
+                .If(HasMedicalRecord)
+                .With(x => x.Address)
+                .Do(CheckAddress)
+                .Return(x => x.PostCode, "UNKNOWN");
         }
 
         private void CheckAddress(Address address)
         {
-            throw new NotImplementedException();
+            Console.WriteLine("CheckAddress method");
         }
 
         private bool HasMedicalRecord(Person person)
         {
-            throw new NotImplementedException();
+            Console.WriteLine("HasMedicalRecord method");
+            return true;
         }
 
         static void Main(string[] args)
         {
-
+            MaybeMonadDemo demo = new MaybeMonadDemo();
+            demo.MyMethod(new Person() { Address = new Address()
+            {
+                PostCode = "513612"
+            },
+                Age = 29, 
+                Name = "Guest"
+            });
         }
     }
 }

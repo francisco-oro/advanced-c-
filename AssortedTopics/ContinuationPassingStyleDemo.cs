51153b1004d91fc1d3be3fdf8c95fcf06089ef18
francisco-oro <francisco.oro@comunidad.unam.mx>
2024-05-07 20:03:43 -0600

    Added continuation Passing style

---
+++ AssortedTopics/ContinuationPassingStyleDemo.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssortedTopics
+{
+    public class QuadraticEquationSolver
+    {
+        // ax^2 + bx + c = 0 
+        public Tuple<Complex, Complex> Start(double a, double b, double c)
+        {
+            var disc = b * b - 4 * a * c;
+            if (disc < 0)
+                return SolveComplex(a, b, c, disc);
+            else
+                return SolveSimple(a, b, c, disc);
+        }
+
+        private Tuple<Complex, Complex> SolveSimple(double d, double d1, double d2, double disc)
+        {
+            var rootDisc = Math.Sqrt(disc);
+            return Tuple.Create(
+                new Complex((-d2 + rootDisc) / (2 * d1), 0),
+                new Complex((-d2 - rootDisc) / (2 * d1), 0));
+
+        }
+
+
+        private Tuple<Complex, Complex> SolveComplex(double d, double d1, double d2, double disc)
+        {
+            var rootDisc = Complex.Sqrt(new Complex(disc, 0));
+            return Tuple.Create(
+                (-d2 + rootDisc) / (2 * d1),
+                (-d2 - rootDisc) / (2 * d1));
+        }
+    }
+    internal class ContinuationPassingStyleDemo
+    {
+        static void Main(string[] args)
+        {
+            var solver = new QuadraticEquationSolver();
+            var solutions = solver.Start(1, 10, 16);
+        }
+    }
+}
---
+++ AssortedTopics/DuckTypingMixinsDemo.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssortedTopics
+{
+    interface IScalar<T> : IEnumerable<T>
+    {
+        IEnumerator<T> IEnumerable<T>.GetEnumerator()
+        {
+            yield return (T)this;
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+    }
+
+    public class MyClass1: IScalar<MyClass1>
+    {
+        public override string ToString()
+        {
+            return "MyClass";
+        }
+    }
+
+    internal class DuckTypingMixinsDemo
+    {
+        public static void Demo(string[] args)
+        {
+            // duck typing 
+
+            // GetEnumerator() -- foreach (IEnumerable<T>)\
+            // Dispose() -- using (IDisposable) 
+            
+
+            // mixin
+
+            var mc = new MyClass1();
+            foreach (var x in mc)
+            {
+                Console.WriteLine(x);
+            }
+        }
+    }
+}

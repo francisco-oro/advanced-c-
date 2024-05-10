9f3eab2dc3dff7680c799336711a47baa71c5277
francisco-oro <francisco.oro@comunidad.unam.mx>
2024-05-09 21:50:49 -0600

    Added Mnemonics to Resharper

--- AssortedTopics/LocalInversionOfControl.cs
+++ AssortedTopics/LocalInversionOfControl.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,6 +9,39 @@ namespace AssortedTopics
 {
     public static class ExtensionMethods
     {
+        public struct BoolMarker<T>
+        {
+            public bool Result;
+            public T Self;
+
+            public enum Operation
+            {
+                None,
+                And,
+                Or
+            }
+
+            internal Operation PendingOp;
+
+            internal BoolMarker(bool result, T self, Operation pendingOp)
+            {
+                Result = result;
+                Self = self; 
+                PendingOp = pendingOp;
+            }
+
+            public BoolMarker(bool result, T self) : this(result, self, Operation.None)
+            {
+            }
+
+            public BoolMarker<T> And => new BoolMarker<T>(Result, Self, Operation.And);
+
+            public static implicit operator bool(BoolMarker<T> marker)
+            {
+                return marker.Result;
+            }
+        }
+
         public static T AddTo<T>(this T self, params ICollection<T>[] colls)
         {
             foreach (ICollection<T> coll in colls)
@@ -22,14 +56,24 @@ namespace AssortedTopics
             return values.Contains(self);
         }
 
-        public static bool HasNo<TSubject, T>(this TSubject self, Func<TSubject, IEnumerable<T>> props)
+        public static BoolMarker<TSubject> HasNo<TSubject, T>(this TSubject self, Func<TSubject, IEnumerable<T>> props)
         {
-            return !props(self).Any();
+            return new BoolMarker<TSubject>(!props(self).Any(), self);
         }
 
-        public static bool HasSome<TSubject, T>(this TSubject self, Func<TSubject, IEnumerable<T>> props)
+        public static BoolMarker<TSubject> HasSome<TSubject, T>(this TSubject self, Func<TSubject, IEnumerable<T>> props)
         {
-            return !props(self).Any();
+            return new BoolMarker<TSubject>(props(self).Any(), self);
+        }
+
+        public static BoolMarker<T> HasNo<T, U>(this BoolMarker<T> marker, Func<T, IEnumerable<U>> props)
+        {
+            if (marker.PendingOp == BoolMarker<T>.Operation.And && !marker.Result)
+            {
+                return marker;
+            }
+
+            return new BoolMarker<T>(!props(marker.Self).Any(), marker.Self);
         }
 
     }
@@ -37,7 +81,7 @@ namespace AssortedTopics
     public class Person
     {
         public List<string> Names = new List<string>();
-        public List<Person> ChildrenList = new List<Person>();
+        public List<Person> Children = new List<Person>();
     }
     public class MyClass2
     {
@@ -61,8 +105,12 @@ namespace AssortedTopics
 
         public void Process(Person person)
         {
+            //if(person.Names.Count == 0) {}
             //if (!person.Names.Any()){}
-            if(person.HasNo(p => p.Names)){}
+            if (person.HasSome(p => p.Names).And.HasNo(p => p.Children))
+            {
+
+            }
         }
     }
     internal class LocalInversionOfControl
---
+++ AssortedTopics/MnemonicsDemo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssortedTopics
+{
+    public class Person1
+    {
+        private double fieldname;
+        public string MyProperty { get; set; }
+        
+    }
+    internal class MnemonicsDemo
+    {
+    }
+}
